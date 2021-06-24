fb86785547b4aaa08a63980b62986be124ade432
Sebbben <sebastian.jossang@gmail.com[A>
2021-06-24 12:56:07 +0200

    basic send message to server working localy

--- client.py
+++ client.py
@@ -1,12 +1,18 @@
 import socket
 
 HEADER = 64
-PORT = 1234
+PORT = 1236
 SERVER = "10.0.0.49"
 ADDR = (SERVER, PORT)
-#ADDR = ('', PORT)
 FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "!Disconnect"
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
+
+msg = ""
+
+while msg != DISCONNECT_MESSAGE:
+    msg = input("Input new message: ")
+    msg = msg.encode(FORMAT)
+    client.send(msg)
--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import threading
 
 
 HEADER = 64
-PORT = 1234
+PORT = 1236
 SERVER = socket.gethostbyname(socket.gethostname())
 # ADDR = (SERVER, PORT)
 ADDR = ('', PORT)
@@ -11,15 +11,26 @@ FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "!Disconnect"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind(ADDR)
 server.listen()
+
+
+def handle_client(client, address):
+    while True:
+        msg = client.recv(1024).decode(FORMAT)
+        print(f"[NEW MESSAGE] from {address} : {msg}")
+        if msg == DISCONNECT_MESSAGE:
+            client.close()
+            return
 
 
 def recive():
     while True:
         client, address = server.accept()
         print(f"[NEW CONNECTION] {address}")
-        client.close()
+        t = threading.Thread(target=handle_client, args=(client, address))
+        t.start()
 
 
 def start():
